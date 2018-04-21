7ee6ffceb0a270fc01d0af7c2355847ab353a627
LZA <318724186>
2018-04-21 14:52:12 +0800

    git_init.sh

---
+++ git_init.sh
@@ -0,0 +1,7 @@
+git config --global user.name "LZA"
+git config --global user.email "318724186"
+
+git init
+#git remote add origin git@github.com:michaelliao/learngit.git
+git remote add script  git@github.com:zhengjue/scripts.git
+#git@github.com:zhengjue/scripts.git
